3ec142de766320c1fde1548e060c6223244723fd
RaulReynoso03 <112824400+RaulReynoso03@users.noreply.github.com>
2022-10-03 16:37:45 +0200

    Add files via upload

---
+++ Metodos_y_Clases/Metodos_y_Clases/Static/Static2/StaticDemo.java
@@ -0,0 +1,9 @@
+package Static2;
+
+public class StaticDemo {
+	static int a = 42;
+	static int b = 99;;
+	static void llamame() {
+	 System.out.println("a = " + a);
+	}
+}
---
+++ Metodos_y_Clases/Metodos_y_Clases/Static/Static2/StaticPorNombre.java
@@ -0,0 +1,9 @@
+package Static2;
+
+public class StaticPorNombre {
+	public static void main(String args[]) {
+		 StaticDemo.llamame();
+		 System.out.println("b ="+ StaticDemo.b);
+		}
+
+}
